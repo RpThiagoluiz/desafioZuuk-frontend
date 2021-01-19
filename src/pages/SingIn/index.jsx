31d391cf7cac7a4ade1fab9c3d55c4dd03d03165
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2021-01-19 17:57:18 -0300

    styles up in signIn Page

--- src/pages/SingIn/index.jsx
+++ src/pages/SingIn/index.jsx
@@ -1,2 +1,20 @@
-const SingIn = () => <h1>Login!</h1>;
+//Styles
+import { Container, Logo, Form, FormTitle, Input, Button } from "./styles";
+//Image
+import appIcon from "../../assets/images/appicon.svg";
+
+const SingIn = () => (
+  <Container>
+    <Logo>
+      <img src={appIcon} alt="" />
+      <h1>Desafio Zukk frontend</h1>
+    </Logo>
+    <Form>
+      <FormTitle>Entrar no App</FormTitle>
+      <Input type="text" placeholder="Usuário"></Input>
+      <Input type="text" placeholder="Senha"></Input>
+      <Button type="submit">Acessar</Button>
+    </Form>
+  </Container>
+);
 export default SingIn;
